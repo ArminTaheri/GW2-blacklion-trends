fdbd2669f74af53e7379f927dcc99a8316986380
Armin Taheri <armin.ytaheri@gmail.com>
2015-10-01 01:19:27 -0400

    Publish application to github.

--- test/gw2_blacklion_trends/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gw2-blacklion-trends.core-test
-  (:require [clojure.test :refer :all]
-            [gw2-blacklion-trends.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
